d4ad0a479d42fa9caf9bbfb73fccb38c1b5e74c4
Emmanuel Blot <emmanuel.blot@free.fr>
2014-09-15 14:39:26 +0200

    Fix #2. Thanks for the report

--- pybootd/tftpd.py
+++ pybootd/tftpd.py
@@ -379,7 +379,7 @@ class TftpConnection(object):
 
     @staticmethod
     def is_url(path):
-        return urlparse.urlsplit(path)['scheme'] and True or False
+        return bool(urlparse.urlsplit(path).scheme)
 
 
 class TftpServer:
